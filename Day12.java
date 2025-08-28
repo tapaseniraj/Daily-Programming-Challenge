af417dacfde07dd2cdb3f526a57fde12f114bd8f
tapaseniraj <148422605+tapaseniraj@users.noreply.github.com>
2025-08-28 18:54:43 +0530

    Add files via upload
    Day 12 Challenge

---
+++ Day12.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Day12 {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("[{()}]"));
+        System.out.println(isValid(""));
+        System.out.println(isValid("{[}"));
+    }
+}
