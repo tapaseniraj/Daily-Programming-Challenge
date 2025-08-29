1060ff86aa3ac386cc31803df9ac7eab9dad96b9
tapaseniraj <148422605+tapaseniraj@users.noreply.github.com>
2025-08-29 21:05:50 +0530

    Add files via upload
    Day 15 Challenge

---
+++ Day15.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Day15 {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring("abcdefgh"));
+        System.out.println(lengthOfLongestSubstring("a"));
+    }
+}
